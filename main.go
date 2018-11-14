9cef96b381c86d1642a718d9f9941b150d6bfd90
Milos Gajdos <milosgajdos83@gmail.com>
2018-11-14 23:26:30 +0000

    Pointers over channels. Default map init. Deadlock removed.
    The previous code had a bug where we attempt to send the inference
    result down the MQTT channel even when we don't use MQTT at all. This
    correctly causes the program to block. We are now checking if the
    MQTT channel has been initialized which is only the case if we use MQTT.
    
    We now also send *pointers* over channels rather than copies of values
    which makes the program less memory extensive and thus less prone to
    running out of memory.
    
    Finally, we now correctly initialize Result Detections map before
    attempting to access its keys when String() is called before the map was
    even initialized leading to program panicing.

--- main.go
+++ main.go
@@ -46,6 +46,20 @@ var (
 	mqttRate int
 )
 
+func init() {
+	flag.IntVar(&deviceID, "device", 0, "Camera device ID")
+	flag.StringVar(&input, "input", "", "Path to image or video file")
+	flag.StringVar(&faceModel, "face-model", "", "Path to .bin file of face detection model")
+	flag.StringVar(&faceConfig, "face-config", "", "Path to .xml file of face model configuration")
+	flag.Float64Var(&faceConfidence, "face-confidence", 0.5, "Confidence threshold for face detection")
+	flag.StringVar(&sentModel, "sent-model", "", "Path to .bin file of sentiment detection model")
+	flag.StringVar(&sentConfig, "sent-config", "", "Path to .xml file of sentiment model configuration")
+	flag.Float64Var(&sentConfidence, "sent-confidence", 0.5, "Confidence threshold for sentiment detection")
+	flag.IntVar(&backend, "backend", 0, "Inference backend. 0: Auto, 1: Halide language, 2: Intel DL Inference Engine")
+	flag.IntVar(&target, "target", 0, "Target device. 0: CPU, 1: OpenCL, 2: OpenCL half precision, 3: VPU")
+	flag.IntVar(&mqttRate, "mqtt-rate", 1, "Number of seconds between data updates to MQTT server")
+}
+
 // Sentiment is shopper sentiment
 type Sentiment int
 
@@ -82,6 +96,19 @@ func (s Sentiment) String() string {
 	}
 }
 
+// Perf stores inference engine performance info
+type Perf struct {
+	// FaceNet stores face detector performance info
+	FaceNet float64
+	// SnetNet stores sentiment detector performance info
+	SentNet float64
+}
+
+// String implements fmt.Stringer interface for Perf
+func (p *Perf) String() string {
+	return fmt.Sprintf("Face inference time: %.2f ms, Sentiment inference time: %.2f ms", p.FaceNet, p.SentNet)
+}
+
 // Result is inference result
 type Result struct {
 	// Shoppers contains count of all detected faces of shoppers
@@ -90,73 +117,60 @@ type Result struct {
 	Detections map[Sentiment]int
 }
 
+// initDetectionsMap initializes Result Detections map to zero values
+func (r *Result) initDetectionsMap() {
+	r.Detections = make(map[Sentiment]int)
+	r.Detections[NEUTRAL] = 0
+	r.Detections[HAPPY] = 0
+	r.Detections[SAD] = 0
+	r.Detections[SURPRISED] = 0
+	r.Detections[ANGER] = 0
+	r.Detections[UNKNOWN] = 0
+}
+
 // String implements fmt.Stringer interface for Result
-func (r Result) String() string {
+func (r *Result) String() string {
+	// if the map is empty, initialize all keys to zero values
+	if r.Detections == nil {
+		r.initDetectionsMap()
+	}
+
 	return fmt.Sprintf("Shoppers: %d, Neutral: %d, Happy: %d, Sad: %d, Surprised: %d, Anger: %d, Unknown: %d",
 		r.Shoppers, r.Detections[NEUTRAL], r.Detections[HAPPY], r.Detections[SAD],
 		r.Detections[SURPRISED], r.Detections[ANGER], r.Detections[UNKNOWN])
 }
 
 // ToMQTTMessage turns result into MQTT message which can be published to MQTT broker
-func (r Result) ToMQTTMessage() string {
+func (r *Result) ToMQTTMessage() string {
+	// if the map is empty, initialize all keys to zero values
+	if r.Detections == nil {
+		r.initDetectionsMap()
+	}
+
 	return fmt.Sprintf("{\"shoppers\": \"%d\", \"neutral\": \"%d\", \"happy\": \"%d\", \"sad\": \"%d\", \"surprised\": \"%d\", \"anger\": \"%d\", \"unknown\": \"%d\"}",
 		r.Shoppers, r.Detections[NEUTRAL], r.Detections[HAPPY], r.Detections[SAD],
 		r.Detections[SURPRISED], r.Detections[ANGER], r.Detections[UNKNOWN])
 }
 
-func init() {
-	flag.IntVar(&deviceID, "device", 0, "Camera device ID")
-	flag.StringVar(&input, "input", "", "Path to image or video file")
-	flag.StringVar(&faceModel, "face-model", "", "Path to .bin file of face detection model")
-	flag.StringVar(&faceConfig, "face-config", "", "Path to .xml file of face detection model configuration")
-	flag.Float64Var(&faceConfidence, "face-confidence", 0.5, "Confidence factor for face detection")
-	flag.StringVar(&sentModel, "sent-model", "", "Path to .bin file of sentiment detection model")
-	flag.StringVar(&sentConfig, "sent-config", "", "Path to .xml file of sentiment detection model configuration")
-	flag.Float64Var(&sentConfidence, "sent-confidence", 0.5, "Confidence factor for sentiment detection")
-	flag.IntVar(&backend, "backend", 0, "Inference backend. 0: Auto, 1: Halide language, 2: Intel DL Inference Engine")
-	flag.IntVar(&target, "target", 0, "Target device. 0: CPU, 1: OpenCL, 2: OpenCL half precision, 3: VPU")
-	flag.IntVar(&mqttRate, "mqtt-rate", 1, "Number of seconds between data updates to MQTT server")
-}
-
-func parseCliFlags() error {
-	// parse cli flags
-	flag.Parse()
-
-	// path to face detection model can't be empty
-	if faceModel == "" {
-		return fmt.Errorf("Invalid path to .bin file of face detection model: %s", faceModel)
-	}
-	// path to face detection model config can't be empty
-	if faceConfig == "" {
-		return fmt.Errorf("Invalid path to .xml file of face detection model configuration: %s", faceConfig)
-	}
-	// path to sentiment detection model can't be empty
-	if sentModel == "" {
-		return fmt.Errorf("Invalid path to .bin file of sentiment detection model: %s", sentModel)
-	}
-	// path to sentiment detection model config can't be empty
-	if sentConfig == "" {
-		return fmt.Errorf("Invalid path to .xml file of sentiment model configuration: %s", sentConfig)
-	}
-
-	return nil
-}
-
 // getPerformanceInfo queries the Inference Engine performance info and returns it as string
-func getPerformanceInfo(faceNet, sentNet gocv.Net, sentChecked bool) string {
+func getPerformanceInfo(faceNet, sentNet gocv.Net, sentChecked bool) *Perf {
 	freq := gocv.GetTickFrequency() / 1000
+
 	facePerf := faceNet.GetPerfProfile() / freq
 	var sentPerf float64
 	if sentChecked {
 		sentPerf = sentNet.GetPerfProfile() / freq
 	}
 
-	return fmt.Sprintf("Face inference time: %.2f ms, Sentiment inference time: %.2f ms", facePerf, sentPerf)
+	return &Perf{
+		FaceNet: facePerf,
+		SentNet: sentPerf,
+	}
 }
 
 // messageRunner reads mqtt message from mqttChan with rate frequency and sends them to MQTT server
 // doneChan is used to receive a signal from the main goroutine to notify messageRunner to stop and return
-func messageRunner(doneChan <-chan struct{}, mqttChan <-chan Result, c *MQTTClient, topic string, rate int) error {
+func messageRunner(doneChan <-chan struct{}, mqttChan <-chan *Result, c *MQTTClient, topic string, rate int) error {
 	ticker := time.NewTicker(time.Duration(rate) * time.Second)
 
 	for {
@@ -183,8 +197,8 @@ func messageRunner(doneChan <-chan struct{}, mqttChan <-chan Result, c *MQTTClie
 
 // frameRunner reads image frames from framesChan and performs face and sentiment detections on them
 // doneChan is used to receive a signal from the main goroutine to notify frameRunner to stop and return
-func frameRunner(framesChan <-chan *gocv.Mat, doneChan <-chan struct{}, resultsChan chan<- Result,
-	perfChan chan<- string, mqttChan chan<- Result, faceNet, sentNet gocv.Net) error {
+func frameRunner(framesChan <-chan *gocv.Mat, doneChan <-chan struct{}, resultsChan chan<- *Result,
+	perfChan chan<- *Perf, mqttChan chan<- *Result, faceNet, sentNet gocv.Net) error {
 	mean := gocv.NewScalar(0, 0, 0, 0)
 
 	for {
@@ -254,17 +268,43 @@ func frameRunner(framesChan <-chan *gocv.Mat, doneChan <-chan struct{}, resultsC
 			}
 
 			// send result down the channel
-			result := Result{
+			result := &Result{
 				Shoppers:   len(faces),
 				Detections: sentMap,
 			}
 			// send data down the channels
 			perfChan <- getPerformanceInfo(faceNet, sentNet, sentChecked)
 			resultsChan <- result
-			mqttChan <- result
+			if mqttChan != nil {
+				mqttChan <- result
+			}
 			img.Close()
 			results.Close()
 		}
+	}
+
+	return nil
+}
+
+func parseCliFlags() error {
+	// parse cli flags
+	flag.Parse()
+
+	// path to face detection model can't be empty
+	if faceModel == "" {
+		return fmt.Errorf("Invalid path to .bin file of face detection model: %s", faceModel)
+	}
+	// path to face detection model config can't be empty
+	if faceConfig == "" {
+		return fmt.Errorf("Invalid path to .xml file of face model configuration: %s", faceConfig)
+	}
+	// path to sentiment detection model can't be empty
+	if sentModel == "" {
+		return fmt.Errorf("Invalid path to .bin file of sentiment detection model: %s", sentModel)
+	}
+	// path to sentiment detection model config can't be empty
+	if sentConfig == "" {
+		return fmt.Errorf("Invalid path to .xml file of sentiment model configuration: %s", sentConfig)
 	}
 
 	return nil
@@ -280,22 +320,22 @@ func main() {
 	// read in Face model and set the backend and target
 	faceNet := gocv.ReadNet(faceModel, faceConfig)
 	if err := faceNet.SetPreferableBackend(gocv.NetBackendType(backend)); err != nil {
-		fmt.Fprintf(os.Stderr, "Error setting backend for Face DNN: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Error setting backend for Face Network: %v\n", err)
 		os.Exit(1)
 	}
 	if err := faceNet.SetPreferableTarget(gocv.NetTargetType(target)); err != nil {
-		fmt.Fprintf(os.Stderr, "Error setting target for Face DNN: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Error setting target for Face Network: %v\n", err)
 		os.Exit(1)
 	}
 
 	// read in Snetiment model and set the backend and target
 	sentNet := gocv.ReadNet(sentModel, sentConfig)
 	if err := sentNet.SetPreferableBackend(gocv.NetBackendType(backend)); err != nil {
-		fmt.Fprintf(os.Stderr, "Error setting backend for Sentiment DNN: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Error setting backend for Sentiment Network: %v\n", err)
 		os.Exit(1)
 	}
 	if err := sentNet.SetPreferableTarget(gocv.NetTargetType(target)); err != nil {
-		fmt.Fprintf(os.Stderr, "Error setting target for Sentiment DNN: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Error setting target for Sentiment Network: %v\n", err)
 		os.Exit(1)
 	}
 
@@ -336,24 +376,17 @@ func main() {
 	// doneChan is used to signal goroutines they need to stop
 	doneChan := make(chan struct{})
 	// resultsChan is used for detection distribution
-	resultsChan := make(chan Result, 1)
-	// mqttChan is used for collecting MQTT stats
-	mqttChan := make(chan Result, 1)
+	resultsChan := make(chan *Result, 1)
 	// perfChan is used for collecting performance stats
-	perfChan := make(chan string, 1)
+	perfChan := make(chan *Perf, 1)
 	// sigChan is used as a handler to stop all the goroutines
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, os.Interrupt, os.Kill, syscall.SIGTERM)
+	// mqttChan is used for collecting MQTT stats; only init if using MQTTT
+	var mqttChan chan *Result
 
 	// waitgroup to synchronise all goroutines
 	var wg sync.WaitGroup
-
-	// start frameRunner goroutine
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		errChan <- frameRunner(framesChan, doneChan, resultsChan, perfChan, mqttChan, faceNet, sentNet)
-	}()
 
 	// create MQTT client and connect to MQTT server
 	opts, err := MQTTClientOptions()
@@ -361,10 +394,11 @@ func main() {
 		fmt.Printf("%v\n", err)
 		fmt.Printf("Have you set the ENV variables?\n")
 	} else {
+		// initialize mqttChan channel here
+		mqttChan = make(chan *Result, 1)
 		mqttClient, err := MQTTConnect(opts)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error connecting to MQTT broker %s: %v\n",
-				os.Getenv("MQTT_SERVER"), err)
+			fmt.Fprintf(os.Stderr, "Failed to connect to MQTT broker %s: %v\n", opts.Servers[0], err)
 			os.Exit(1)
 		}
 		// start MQTT worker goroutine
@@ -376,11 +410,19 @@ func main() {
 		defer mqttClient.Disconnect(100)
 	}
 
-	// label will contain
-	var label string
+	// start frameRunner goroutine
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		errChan <- frameRunner(framesChan, doneChan, resultsChan, perfChan, mqttChan, faceNet, sentNet)
+	}()
+
+	// initialize the result pointers
+	result := new(Result)
+	perf := new(Perf)
+
 monitor:
 	for {
-		// read image frames from image capture source
 		if ok := vc.Read(&img); !ok {
 			fmt.Printf("Cannot read image source %v\n", deviceID)
 			break
@@ -388,7 +430,7 @@ monitor:
 		if img.Empty() {
 			continue
 		}
-		// send the captured frame for detection
+
 		framesChan <- &img
 
 		select {
@@ -396,24 +438,23 @@ monitor:
 			fmt.Printf("Shutting down. Got signal: %s\n", sig)
 			break monitor
 		case err = <-errChan:
-			fmt.Printf("Shutting down. Failed with error: %s\n", err)
+			fmt.Printf("Shutting down. Encountered error: %s\n", err)
 			break monitor
-		case result := <-resultsChan:
-			// retrieve the inference performance and print it
-			label = <-perfChan
-			gocv.PutText(&img, label, image.Point{0, 15}, gocv.FontHersheySimplex, 0.5,
-				color.RGBA{0, 0, 0, 0}, 2)
-			// print the inference results label
-			label = fmt.Sprintf("%s", result)
-			gocv.PutText(&img, label, image.Point{0, 40}, gocv.FontHersheySimplex, 0.5,
-				color.RGBA{0, 0, 0, 0}, 2)
+		case result = <-resultsChan:
+			perf = <-perfChan
+		default:
+			// do nothing; just display latest results
 		}
-
+		// inference performance and print it
+		gocv.PutText(&img, fmt.Sprintf("%s", perf), image.Point{0, 15},
+			gocv.FontHersheySimplex, 0.5, color.RGBA{0, 0, 0, 0}, 2)
+		// inference results label
+		gocv.PutText(&img, fmt.Sprintf("%s", result), image.Point{0, 40},
+			gocv.FontHersheySimplex, 0.5, color.RGBA{0, 0, 0, 0}, 2)
 		// show the image in the window, and wait 1 millisecond
 		window.IMShow(img)
 		window.WaitKey(1)
 	}
-
 	// signal all goroutines to finish
 	close(doneChan)
 	// wait for all goroutines to finish
